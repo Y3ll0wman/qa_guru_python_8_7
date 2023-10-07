34655af1d056af57509711ce048a71f2a7aa536b
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-10-07 18:48:20 +0300

    Убрал из теста test_files_should_be_zipped хардкод имен файлов

--- tests/test_files_should_be_zipped.py
+++ tests/test_files_should_be_zipped.py
@@ -20,22 +20,40 @@ def test_files_should_be_zipped():
                 # Добавляем файл в архив с его относительным путем
                 myzip.write(file_path, relative_path)
 
+    # Записать в files_in_resources список файлов из каталога /resources
+    files_in_resources = os.listdir(RESOURCES_PATH)
+
     # Записать в files_in_archive список файлов из архива /tmp/resources.zip
     with (ZipFile(f'{TMP_PATH}/resources.zip') as myzip):
         files_in_archive = myzip.namelist()
+
+    # Проитерировать список и вернуть словарь с ключами тип файла
+    resources = {}
+    for file_name in files_in_resources:
+        if '.xlsx' in file_name:
+            resources['xlsx'] = f'{file_name}'
+        elif '.xls' in file_name:
+            resources['xls'] = f'{file_name}'
+        elif '.zip' in file_name:
+            resources['zip'] = f'{file_name}'
+        elif '.pdf' in file_name:
+            resources['pdf'] = f'{file_name}'
 
     # Удалить архив /tmp/resources.zip
     os.remove(f'{TMP_PATH}/resources.zip')
 
     # Проверить, что файл с именем "Python Testing with Pytest (Brian Okken).pdf" помещен в архив
-    assert 'Python Testing with Pytest (Brian Okken).pdf' in files_in_archive, \
-            f"Файл 'Python Testing wi2th Pytest (Brian Okken).pdf' не найден в архиве"
+    assert resources['pdf'] in files_in_archive, \
+        f"Файл {resources['pdf']} не найден в архиве"
 
     # Проверить, что файл с именем "file_example_XLSX_50.xlsx" помещен в архив
-    assert 'file_example_XLSX_50.xlsx' in files_in_archive, f"Файл 'file_example_XLSX_50.xlsx' не найден в архиве"
+    assert resources['xlsx'] in files_in_archive, \
+        f"Файл {resources['xlsx']} не найден в архиве"
 
     # Проверить, что файл с именем "file_example_XLS_10.xls" помещен в архив
-    assert 'file_example_XLS_10.xls' in files_in_archive, f"file_example_XLS_10.xls' не найден в архиве"
+    assert resources['xls'] in files_in_archive, \
+        f"Файл {resources['xls']} не найден в архиве"
 
     # Проверить, что файл с именем "hello.zip" помещен в архив
-    assert 'hello.zip' in files_in_archive, f"hello.zip' не найден в архиве"
+    assert resources['zip'] in files_in_archive, \
+        f"Файл {resources['zip']} не найден в архиве"
